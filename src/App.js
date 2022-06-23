c79629d6b944e7489936e462fa48c4b19d3cd18e
ElideZavala <zavalavinagreelide@gmail.com>
2022-06-23 18:40:25 -0500

    API

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import MovieList from "./components/MovieList";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import initialMovies from '../consts/initialMovies'
+import Movie from './Movie'
 
 const MovieList = () => {
   return (
-	 <div>
-		<h1>MovieList</h1>
+	 <div className='container'>
+		<div className='row'>
+			{initialMovies.map(movie => (
+				<div className='col-md-4' key={movie.id}>
+					<Movie
+						movie={movie}
+					/>
+				</div>
+			))}
+		</div>
 	 </div>
   )
 }
