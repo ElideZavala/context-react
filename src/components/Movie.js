e2f3f099ab8eace51152f1ca5db3a6d3f67aa4ae
ElideZavala <zavalavinagreelide@gmail.com>
2022-06-30 09:16:29 -0500

    Creacion de componente Movie

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,15 +1,19 @@
-import React from 'react'
+import { useContext }  from 'react'
+import UserContext from '../contexts/UserContext'
 
 const Movie = ({ movie }) => {
+	const { user, toggleFavoriteMovieToUser } = useContext(UserContext); 
 
 	const imgStyles = {
 		height: '290px',
 		objectFit: 'cover',
 	}
 
-	const isFavorite = false;
-
-  return (
+	// Comprovar si este arrglo contiene esta pelicula, si es sierto la variable sera True(success)
+	// Antes de hacer una operacion sobre un arreglo es recomendable colocar el signo ?
+	const isFavorite = user?.favoriteMovie?.includes(movie.id);
+ 
+	return (
 	 <div className='card'>
 		<img
 			src={movie.imageUrl}
@@ -19,11 +23,15 @@ const Movie = ({ movie }) => {
 		/>
 		<div className='card-body'>
 			<h4>{movie.title}</h4>
-			<button className={`btn ${isFavorite ? 'btn-success' : 'btn-outline-primary'}`}>
+			{ user?.id &&
+			<button className={
+				`btn ${isFavorite ? 'btn-success' : 'btn-outline-primary'}`}
+				onClick={()=> toggleFavoriteMovieToUser(movie.id)}
+			>
 				Favorito
 			</button>
+			}
 		</div>
-		<h1>Movie</h1>
 	 </div>
   )
 }
